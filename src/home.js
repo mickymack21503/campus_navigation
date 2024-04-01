9a9b5e01cf2b95492b54229a981788c874c06a34
mickymack21503 <98085850+mickymack21503@users.noreply.github.com>
2024-04-02 00:17:28 +0530

    Update home.js

--- src/home.js
+++ src/home.js
@@ -1,13 +1,17 @@
+// Home.js
+
 import React from 'react';
-import Header from './header';
-import Herosection from './Herosection'
-function Home(){
-    return(
-        <>
-        <Header/>
-        <Herosection/>
-        </>
-    );
+import { Link } from 'react-router-dom';
+
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Link to="/explore">
+        <button>Start Exploring</button>
+      </Link>
+    </div>
+  );
 }
 
 export default Home;
