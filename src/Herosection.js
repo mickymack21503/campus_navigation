286cde0cc9476515fab9b3a06f04d699616bcd89
mickymack21503 <98085850+mickymack21503@users.noreply.github.com>
2024-04-02 09:49:36 +0530

    Update Herosection.js

--- src/Herosection.js
+++ src/Herosection.js
@@ -4,33 +4,39 @@ import { Link } from "react-router-dom";
 function Herosection() {
   return (
     <div>
-      <div class="pt-4">
-        <h1 class="text-xl text-center font-bold p-2">
+      <div className="pt-4">
+        <h1 className="text-xl text-center font-bold p-2">
           Welcome to Ganpat University Campus
         </h1>
-        <h1 class="text-center px-2 pt-2 pb-6">
+        <h1 className="text-center px-2 pt-2 pb-6">
           Explore our university campus and discover all the amazing places it
           has to offer.
         </h1>
 
         <center>
-          <button class="bg-black text-white border-white borde rounded-lg font-bold py-2 px-4 hover:bg-slate-900">
-            Start Exploring
-          </button>
+          {/* Use Link component to navigate to Explore page */}
+          <Link to="/explore">
+            <button className="bg-black text-white border-white border rounded-lg font-bold py-2 px-4 hover:bg-slate-900">
+              Start Exploring
+            </button>
+          </Link>
         </center>
       </div>
 
-      <div class="p-5">
+      <div className="p-5">
         <center>
-          <img src="./guni_main.jpg" class="max-h-52" alt="main"></img>
-          <h1 class="text-2xl font-bold p-2">Main Building</h1>
-          <h3 class="text-lg p-2">
+          <img src="./guni_main.jpg" className="max-h-52" alt="main"></img>
+          <h1 className="text-2xl font-bold p-2">Main Building</h1>
+          <h3 className="text-lg p-2">
             The heart of our campus, where all the administrative offices are
             located.
           </h3>
-          <button class="bg-black text-white border-white borde rounded-lg font-bold py-2 px-4 hover:bg-slate-900">
-            Visit{" "}
-          </button>
+          {/* You can also add Link component for the "Visit" button if needed */}
+          {/* <Link to="/explore">
+            <button className="bg-black text-white border-white border rounded-lg font-bold py-2 px-4 hover:bg-slate-900">
+              Visit
+            </button>
+          </Link> */}
         </center>
       </div>
     </div>
